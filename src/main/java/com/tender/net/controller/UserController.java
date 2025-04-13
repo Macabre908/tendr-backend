112410d18b627356e4fc55cf15567b8d5724af4e
kaf <abhishekdubey789901@gmail.com>
2025-04-14 00:43:06 +0530

    Created passwordEncoder for Password Hashing Created ToolsConfig.java for Bean Creation Established db connection in the application.properties file.

--- src/main/java/com/tender/net/controller/UserController.java
+++ src/main/java/com/tender/net/controller/UserController.java
@@ -5,6 +5,7 @@ import com.tender.net.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "http://localhost:5173")
@@ -15,9 +16,12 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @PostMapping("/userSignUp")
     public ResponseEntity<HttpStatus> userSignUp(@RequestBody User user) {
-        userRepository.save(user);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
--- src/main/java/com/tender/net/entities/User.java
+++ src/main/java/com/tender/net/entities/User.java
@@ -8,10 +8,8 @@ import lombok.Setter;
 
 @Entity
 @Table(name = "user_detail")
-@Getter
-@Setter
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
     @Id
@@ -33,4 +31,59 @@ public class User {
     @Column(name = "location")
     private String location;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public User(String name, String email, String password, String phoneNumber, String location) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.location = location;
+    }
 }
